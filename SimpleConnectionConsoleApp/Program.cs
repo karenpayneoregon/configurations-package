e7be2e37a98c979a7aa3799d73301ef4990f9836
Karen Payne <paynekaren@comcast.net>
2022-08-02 07:15:11 -0700

    Various

---
+++ SimpleConnectionConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConfigurationLibrary.Classes;
+
+namespace SimpleConnectionConsoleApp
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationHelper.ConnectionString();
+            Console.WriteLine(connectionString);
+            Console.ReadLine();
+        }
+    }
+}
