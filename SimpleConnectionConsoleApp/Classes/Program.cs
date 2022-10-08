c2a25ff3db4ad9608b7237883a21381900b24559
Karen Payne <paynekaren@comcast.net>
2022-10-08 11:18:51 -0700

    done

--- SimpleConnectionConsoleApp/Classes/Program.cs
+++ SimpleConnectionConsoleApp/Classes/Program.cs
@@ -13,34 +13,6 @@ namespace SimpleConnectionConsoleApp
             Console.Title = "Code sample";
         }
     }
-
-
-    public class AspCoreRoot
-    {
-        public Logging Logging { get; set; }
-        public string AllowedHosts { get; set; }
-        public ConnectionsConfiguration ConnectionsConfiguration { get; set; }
-    }
-
-    public class Logging
-    {
-        public LogLevel LogLevel { get; set; }
-    }
-
-    public class LogLevel
-    {
-        public string Default { get; set; }
-        public string MicrosoftAspNetCore { get; set; }
-    }
-
-    public class ConnectionsConfiguration
-    {
-        public string ActiveEnvironment { get; set; }
-        public string Development { get; set; }
-        public string Stage { get; set; }
-        public string Production { get; set; }
-    }
-
 }
 
 
