7718f23a959b9a80346f0fd654968b85822947c2
Karen Payne <paynekaren@comcast.net>
2022-10-13 03:47:37 -0700

    clean-up

--- ConfigurationLibrary/Classes/ConfigurationMapAsp.cs
+++ ConfigurationLibrary/Classes/ConfigurationMapAsp.cs
@@ -9,15 +9,6 @@ namespace ConfigurationLibrary.Classes
         public string Production { get; set; }
     }
 
-
-    public class Rootobject
-    {
-        public Logging Logging { get; set; }
-        public string AllowedHosts { get; set; }
-        public ConnectionsConfiguration ConnectionsConfiguration { get; set; }
-        public ConnectionStrings ConnectionStrings1 { get; set; }
-    }
-
     public class Logging
     {
         public LogLevel LogLevel { get; set; }
@@ -29,14 +20,5 @@ namespace ConfigurationLibrary.Classes
         public string MicrosoftAspNetCore { get; set; }
     }
 
-
-
-    public class ConnectionStrings
-    {
-        public string ActiveEnvironment { get; set; }
-        public string Development { get; set; }
-        public string Stage { get; set; }
-        public string Production { get; set; }
-    }
 
 }
